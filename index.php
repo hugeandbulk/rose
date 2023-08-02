50b23c0efa93910c3e13cd4cb0a1b5fd40f85749
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-08-02 20:10:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,16 @@ echo "rose, a php based system!";
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create rose application.
+| CEO //
+| Concept & Idea //
+| Principal Architect //
 | Creator   : ¦  Prabhat Kumar  ¦
+|--------------------------------
 | Copyright : ¦  Prabhat Kumar  ¦
+|--------------------------------
 | Company   : ¦  Infinity, Inc. ¦
+|--------------------------------
 | COO       : ¦  Zareen Khan    ¦
 */
-/* ————eof-— */
+/* ———— eof-— */
 ?>
